37691774e77e0dbf6d37ec273a5a39b5d372b1d4
nikhilchowdary1918 <nikhilchowdary1918@gmail.com>
2024-03-11 19:38:38 -0400

    Small changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
 import { resList } from "../Constants";
 import Restcard from "./Restcard";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 function filterData(searchInput,restInput){
   const filterData= restInput.filter((restaurant) => restaurant.data.name.includes(searchInput));
@@ -10,6 +10,16 @@ const Body= () =>{
     const[restInput,setRestInput]=useState(resList)
     const[searchInput,setSearchInput]=useState("")
 
+    useEffect(()=>{getRestfromapi();},[])
+    //useEffect(()=>{console.log("use effect is called")},[searchInput]) example of useeffect when searchinput is changed
+    //useEffect(()=>{console.log("use effect is called")},[]) If dependancy array is empty ,it is called only once
+
+    async function getRestfromapi(){
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9622536&lng=77.6979885&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      const json = await data.json();
+      console.log(json)
+      //setRestInput(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0].info)
+    }
     return(
       <>
       <div className="search-container">
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,13 @@ const Title = () => {
   );
 };
 const Header = () => {
-  const[ttl,setTtl]=useState("food heaven")
+
   return (
 
     <>
       <div className="header">
         <Title />
-        <h1>{ttl}</h1>
-        <button onClick={()=> setTtl("Food villa changed")}>Click to change title</button>
+
         <div className="nav-items">
           <ul>
             <li>Home</li>
