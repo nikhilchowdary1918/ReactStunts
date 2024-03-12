34ae67ae15df3e29318c18ac85e9c3580c658dc4
nikhilchowdary1918 <nikhilchowdary1918@gmail.com>
2024-03-11 23:36:36 -0400

    Shimmer

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,15 @@
 import { resList } from "../Constants";
 import Restcard from "./Restcard";
 import { useState,useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 function filterData(searchInput,restInput){
   const filterData= restInput.filter((restaurant) => restaurant.data.name.includes(searchInput));
   return filterData;
 }
 const Body= () =>{
-    const[restInput,setRestInput]=useState(resList)
+    const[restInput,setRestInput]=useState([])
+    //restInput--Data in cards, setRestdata-- function that is created by useState hook to change the state of the data
     const[searchInput,setSearchInput]=useState("")
 
     useEffect(()=>{getRestfromapi();},[])
@@ -18,9 +20,17 @@ const Body= () =>{
       const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9622536&lng=77.6979885&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
       const json = await data.json();
       console.log(json)
-      //setRestInput(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0].info)
-    }
-    return(
+      const arrayOfCards = json.data.cards; 
+      const restaurant_list = "restaurant_grid_listing";
+      for (const cardObj of arrayOfCards) { 
+        if (cardObj.card.card && cardObj.card.card.id === restaurant_list) 
+        { 
+          const resData = cardObj.card?.card?.gridElements?.infoWithStyle?.restaurants;
+          console.log(resData)
+          setRestInput(resData)
+      //setRestInput(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }}}
+    return(restInput.length === 0)? <Shimmer/> : (
       <>
       <div className="search-container">
         <input 
@@ -45,13 +55,13 @@ const Body= () =>{
  
       </div>
       <div className="restcard">
-        {
-          restInput.map((restaurant)=>{
-            return <Restcard {...restaurant.data} key={restaurant.data.id}/>
-          })
-        }
-    
+      {
+        restInput.map((restaurant) => (
+        restaurant.info ? <Restcard {...restaurant.info} key={restaurant.info.id}/> : null
+        ))
+      }
       </div>
+
       </>
     )
     }
---
+++ src/components/Shimmer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ShimmerSimpleGallery } from "react-shimmer-effects";
+
+const Shimmer=() => {
+    return (
+      <>
+        <ShimmerSimpleGallery card imageHeight={300} />
+      </>
+    );
+  }
+  export default Shimmer;
